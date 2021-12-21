3b18b4dd794bd799c4f1f138e82b8b52a1f5bdcc
UffeMan <92825162+UffeMan@users.noreply.github.com>
2021-12-21 05:07:05 +0100

    Alla länkar startar från roten

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
 $(function () {
-    $("#header").load("../html/header.html");
-    $("#footer").load("../html/footer.html");
+    $("#header").load("../Documents/header.html");
+    $("#footer").load("../Documents/footer.html");
 });
