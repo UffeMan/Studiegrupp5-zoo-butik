b374f8b3df64d894e0cd33aaad30387f908e625b
olga <oklimovich37@gmail.com>
2021-12-29 17:28:11 +0100

    JS för hundvalpar

--- js/app.js
+++ js/app.js
@@ -3,4 +3,32 @@ $(function () {
     $("#footer").load("../Documents/footer.html");
 });
 
-alert("hej")
+/*alert("hej")*/
+
+const infoBlock=document.getElementById("puppyInfo");
+function valpOne() {
+    infoBlock.style.display="flex";
+} 
+
+function valpTwo() {
+    infoBlock.style.display="flex";
+} 
+
+function valpThree() {
+    infoBlock.style.display="flex";
+} 
+
+function valpFour() {
+    infoBlock.style.display="flex";
+} 
+
+function valpFive() {
+    infoBlock.style.display="flex";
+} 
+
+function closeBlock(){
+    infoBlock.style.display="none";
+}
+
+ 
+
